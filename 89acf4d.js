a6c7326ae1f5dabccadb834c1d2932de354adbfa
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-29 10:22:05 +0900

    Add files via upload

---
+++ 89acf4d.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/fkep3@main/mainbg_loop.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
